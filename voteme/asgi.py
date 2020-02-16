53e628e3d30664c330dfc69d08e7fcd5e80feccf
Henrik Boelte <unknownwowde1@web.de>
2020-02-16 16:11:25 -0500

    Init

---
+++ voteme/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for voteme project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'voteme.settings')
+
+application = get_asgi_application()
